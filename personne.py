a2bc5f0e8aa7a64499efb4b42dd52bb0a3dc2c15
Grégoire Antoine <he201703@students.ephec.be>
2020-11-23 09:43:10 +0100

    Commentaire fichier Personne.py

--- personne.py
+++ personne.py
@@ -1,6 +1,7 @@
-#poulet poulet
 
+#initalisation de la classe Personne reprenant toutes les infos sur une personne
 class Personne():
+    #initalisation des informations nécéssaire
     def __init__(self, prenom, nom, dateNaissance = "--/--/----", mail = "mail@mail.com", telephone = "0000 00 00 00", adresse = "---"):
         self._prenom = prenom
         self._nom = nom
@@ -8,10 +9,11 @@ class Personne():
         self._mail = mail
         self._telephone = telephone
         self.adresse = adresse
-
+#définition du format
     def __repr__(self):
         return "{} | {} | {} | {} | {} | {}".format(self._prenom, self._nom, self._dateNaissance, self._mail, self._telephone, self._adresse)
 
+#Getter et setter du prénom
     def _get_prenom(self):
         return self._prenom
 
@@ -20,6 +22,7 @@ class Personne():
 
     prenom = property(_get_prenom, _set_prenom)
 
+#Getter et setter du nom
     def _get_nom(self):
         return self._nom
 
@@ -28,6 +31,7 @@ class Personne():
 
     nom = property(_get_nom, _set_nom)
 
+#Getter et setter de la date de naissance
     def _get_dateNaissance(self):
         return self._dateNaissance
 
@@ -36,6 +40,7 @@ class Personne():
 
     dateNaissance = property(_get_dateNaissance, _set_dateNaissance)
 
+#Getter et setter du mail
     def _get_mail(self):
         return self._mail
 
@@ -44,6 +49,7 @@ class Personne():
 
     mail = property(_get_mail, _set_mail)
 
+#Getter et setter du téléphone
     def _get_telephone(self):
         return self._telephone
 
@@ -52,6 +58,7 @@ class Personne():
 
     telephone = property(_get_telephone, _set_telephone)
 
+#Getter et setter du l'adresse
     def _get_adresse(self):
         return self._adresse
 
