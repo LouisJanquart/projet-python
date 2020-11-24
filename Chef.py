11f550faae65a40fc7c1ed8bab55c039b4defe52
Grégoire Antoine <he201703@students.ephec.be>
2020-11-24 14:38:09 +0100

    class chef

---
+++ Chef.py
@@ -0,0 +1,10 @@
+
+class Chef ():
+    def __init__(self, dateDebut):
+        self._dateDebut = dateDebut
+
+     def _get_DateDebut(self):
+        return self._dateDebut
+
+    def _set_DateDebut(self, dateDebut):
+        self._dateDebut = dateDebut
