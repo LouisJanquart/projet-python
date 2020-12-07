2fdc773c012f010ba7e925a15d01e325b3553712
Grégoire Antoine <he201703@students.ephec.be>
2020-12-07 09:08:26 +0100

    interface graphique

---
+++ interface graphique projet/fonctionstableaux.py
@@ -0,0 +1,24 @@
+def tab_nutons():
+ 
+  print(" tableaux des Nutons")
+
+def tab_lutins():
+    print(" tableaux des lutins")
+
+def tab_louveteaux():
+    print(" tableaux des louveteaux")
+
+def tab_guides():
+    print(" tableaux des guides")
+
+def tab_scouts():
+    print(" tableaux des scouts")
+
+def tab_pionniers():
+    print(" tableaux des pionniers")
+
+def tab_chefs():
+    print(" tableaux des chefs")
+
+def test():
+    print(" test")
