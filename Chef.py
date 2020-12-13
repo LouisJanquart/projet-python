65655284f5d9fe4c566cd22e3df32cfda56142d7
Grégoire Antoine <he201703@students.ephec.be>
2020-12-13 14:56:51 +0100

    interface

--- Chef.py
+++ Chef.py
@@ -1,10 +1,5 @@
+def mafunction (issou) :
+    print(issou+3) 
 
-class Chef ():
-    def __init__(self, dateDebut):
-        self._dateDebut = dateDebut
 
-     def _get_DateDebut(self):
-        return self._dateDebut
-
-    def _set_DateDebut(self, dateDebut):
-        self._dateDebut = dateDebut
+mafunction(5)
